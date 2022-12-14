49594ec29eeca0cc2b34eb46eb597cb4bf07ea06
Fahd Bhatty <fahd.bhatty@gmail.com>
2022-12-13 20:00:26 -0600

    API complete but without authentication

---
+++ FlightPlanApi/Controllers/FlightPlanController.cs
@@ -0,0 +1,111 @@
+﻿using FlightPlanApi.Data;
+using FlightPlanApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanApi.Controllers
+{
+    [Route("api/v1/flightplan")]
+    [ApiController]
+    public class FlightPlanController : ControllerBase
+    {
+        private IDatabaseAdapter _database;
+
+        public FlightPlanController(IDatabaseAdapter database)
+        {
+            _database = database;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FlightPlanList()
+        {
+            var flightPlanList = await _database.GetAllFlightPlans();
+            if (flightPlanList.Count == 0)
+                return NoContent(); //return 204
+            return Ok(flightPlanList); //returns 200 and flight plan list in body
+        }
+
+        [HttpGet]
+        [Route("{flightPlanId}")]
+        public async Task<IActionResult> GetFlightPlanById(string flightPlanId)
+        {
+            var flightPlan = await _database.GetFlightPlanById(flightPlanId);
+            if (flightPlan.FlightPlanId != flightPlanId)
+                return StatusCode(StatusCodes.Status404NotFound);
+            return Ok(flightPlan);
+        }
+
+        [HttpPost]
+        [Route("file")] //file is part of the route path. it isnt a variable or any special flag or anything
+        public async Task<IActionResult> FileFlightPlan(FlightPlan flightPlan)
+        {
+            var transactionResult = await _database.FileFlightPlan(flightPlan);
+            switch (transactionResult)
+            {
+                case TransactionResult.Success:
+                    return Ok();
+                case TransactionResult.BadRequest:
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                default:
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateFlightPlan(FlightPlan flightPlan)
+        {
+            var transactionResult = await _database.FileFlightPlan(flightPlan);
+            switch (transactionResult)
+            {
+                case TransactionResult.Success:
+                    return Ok();
+                case TransactionResult.BadRequest:
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                default:
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{flightPlanId}")]
+        public async Task<IActionResult> DeleteFlightPlan(string flightPlanId)
+        {
+            var result = await _database.DeleteFlightPlanById(flightPlanId);
+            if (result)
+                return Ok();
+            return StatusCode(StatusCodes.Status404NotFound);
+        }
+
+        [HttpGet]
+        [Route("airport/departure/{flightPlanId}")]
+        public async Task<IActionResult> GetFlightPlanDepartureAirport(string flightPlanId)
+        {
+            var flightPlan = await _database.GetFlightPlanById(flightPlanId);
+            if (flightPlan.FlightPlanId != flightPlanId)
+                return StatusCode(StatusCodes.Status404NotFound);
+            return Ok(flightPlan.DepartureAirport);
+        }
+
+        [HttpGet]
+        [Route("route/{flightPlanId}")]
+        public async Task<IActionResult> GetFlightPlanRoute(string flightPlanId)
+        {
+            var flightPlan = await _database.GetFlightPlanById(flightPlanId);
+            if (flightPlan.FlightPlanId != flightPlanId)
+                return StatusCode(StatusCodes.Status404NotFound);
+            return Ok(flightPlan.Route);
+        }
+
+        [HttpGet]
+        [Route("time/enroute/{flightPlanId}")]
+        public async Task<IActionResult> GetFlightPlanTimeEnroute(string flightPlanId)
+        {
+            var flightPlan = await _database.GetFlightPlanById(flightPlanId);
+            if (flightPlan.FlightPlanId != flightPlanId)
+                return StatusCode(StatusCodes.Status404NotFound);
+
+            var estimatedTimeRoute = flightPlan.ArrivalTime - flightPlan.DepartureTime;
+            return Ok(estimatedTimeRoute); 
+        }
+    }
+}
--- FlightPlanApi/Program.cs
+++ FlightPlanApi/Program.cs
@@ -1,3 +1,5 @@
+using FlightPlanApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+builder.Services.AddScoped<IDatabaseAdapter, MongoDbDatabase>(); 
 
 var app = builder.Build();
 
@@ -15,6 +19,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(config =>
+{
+    config.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+});
 
 app.UseHttpsRedirection();
 
